be2dfb00a16acd1ca0915bd78804a298b4f47a3b
Kaneaug <kane.monaco@gmail.com>
2023-01-27 20:27:26 -0800

    uploading final file with aes changes

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(dplyr)
 library(stringr)
 library(DT)
+library(shinythemes)
 df_main <- read.csv("C://Users/kanem/Rshiny_project/Rshiny_project/csv_combine/objects.csv")
 df<- read.csv("C://Users/kanem/Rshiny_project/Rshiny_project/csv_combine/combined_csv.csv")
 fund<- read.csv("C://Users/kanem/Rshiny_project/Rshiny_project/csv_combine/funds.csv")
@@ -20,26 +21,34 @@ df$year_df <- df$acquired_at %>%
 df$color <- df$term_code
 tot_fun <- sum(df_main$funding_total_usd)
 
+
 ui <- fluidPage(
-    tags$head(
-      tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.min.css")),
+    theme = shinytheme("sandstone"),
     
     titlePanel("Start-up Market Insights"),
     sidebarLayout(
       sidebarPanel(
     selectInput("op_status", "Company's Operation Status", choices = unique(df_main$status)),
-    sliderInput("rounds", "Rounds of funding", min=1, max=15, value=1)),
+    sliderInput("rounds", "Rounds of funding", min=1, max=15, value=1),
+    p("Average Rounds of Funding Per Company: 1.3"),
+    p("Average Funding Per Company: $11,205,961"),
+    p("Median Funding Per Company: $1,000,000")),
     mainPanel(
       tabsetPanel(
-        tabPanel("Funding by Industry", plotOutput("fund_plot"),p("Total funding for every industry from start-up investors.
-        Startup investors make a profit from their investments when they sell part or all of 
-        their portion of ownership in the company during a liquidity event, such as an IPO or acquisition. Now, however, 
-        most of the value creation has shifted to early-stage private company investors.
-        In fact, if you were to wait until a startup goes public to invest, you could be 
-        missing out on 95% of the gains, which are often accrued by investors before the IPO.")),
+        tabPanel("Funding by Industry", plotOutput("fund_plot"),p("Investing in Start-Up companies can be a very lucrative investment.However as Craig
+                                                                  Peter of Growth Capital Ventures states 'it's essential to build a portfolio of companies. 
+                                                                  It's sensible to invest in 5 to 10 startups to ensure there's a solid approach to portfolio 
+                                                                  diversification. Across a portfolio of 10 investments, it's most likely two or three will 
+                                                                  perform very well, another two or three will break even and the others will fail. Overall, 
+                                                                  the gains made from the high-performing portfolio 
+                                                                  companies outweigh the downside of the non-performing assets.'")),
         tabPanel("Company overview", DT::dataTableOutput("d_table")),
         tabPanel("Total Funding", plotOutput("sc_plot"),paste("Total Funding in USD", "$", tot_fun)), 
-        tabPanel("Analysis", plotOutput("line_g"))
+        tabPanel("Analysis", plotOutput("line_g"),
+                 p("Of the 6,613 U.S.-based companies initially funded by venture capital between
+                 2006 and 2011, 84% now are closely held and operating independently, 11% were acquired
+                 or made initial public offerings 
+                   of stock and 4% went out of business, according to Dow Jones VentureSource"))
       )
       
     )
@@ -71,7 +80,7 @@ server <- function(input, output, session) {
       labs(x="Total Capital Raised")
   })
 output$d_table <- DT::renderDataTable({
-  f_table <- df_main[, c("name","category_code","status","homepage_url","description","overview")]
+  f_table <- df_main[, c("name","category_code","status","homepage_url","description","overview", "funding_total_usd")]
   f_table
 })
 output$line_g <- renderPlot({
